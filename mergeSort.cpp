4ede2d2537f4128ca8c1281530767bf31bd5648b
abu jafar <abujafarshiddik123@gmail.com>
2023-01-19 10:55:18 +0600

    mergeSort modified

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> merge_sort(vector<int> a)
+vector<int> merge_sort(vector<int> a,int k)
 {
+    int sum = 0;
     if (a.size() <= 1)
         return a;
     int mid = a.size() / 2;
@@ -18,34 +19,20 @@ vector<int> merge_sort(vector<int> a)
     int idx2 = 0;
     for (int i = 0; i < a.size(); i++)
     {
-        if (idx1 == sorted_b.size())
-        {
-            sorted_a.push_back(sorted_c[idx2]);
-            idx2++;
-        }
-        else if (idx2 == sorted_c.size())
-        {
-            sorted_a.push_back(sorted_b[idx1]);
-            idx1++;
-        }
-        else if (sorted_b[idx1] < sorted_c[idx2])
-        {
-            sorted_a.push_back(sorted_b[idx1]);
-            idx1++;
-        }
-        else
-        {
-            sorted_a.push_back(sorted_c[idx2]);
-            idx2++;
-        }
+
     }
-    return sorted_a;
+    return sum;
 }
 int main()
 {
-    vector<int> a = {2, 5, 1, 7, 3, 2, 4};
-    vector<int> result = merge_sort(a);
-    for (int i = 0; i < result.size(); i++)
-        cout << result[i] << " ";
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for (int i = 0; i < a.size(); i++)
+    {
+        cin>>a[i];
+    }
+    vector<int> result = merge_sort(a,k);
+    cout<<result.size();
     return 0;
 }
